c61b25e3a733d153dfaaf878ce5fb6175829cc84
denisero <79418860+denisero21@users.noreply.github.com>
2023-07-05 14:22:06 +0300

    final

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from open_theory import open_pdf
 if __name__ == "__main__":
     cursor = connection.cursor()
     current_user_node = 1  # В будущем подтянуть из бд
+    cursor.execute(f"SELECT * FROM nodes")
+    nodes = cursor.fetchall()
 
-    while True:
+
+    while current_user_node <= len(nodes):
         open_pdf(cursor, current_user_node)
         if check_questions(cursor, current_user_node):
             current_user_node += 1
--- open_theory.py
+++ open_theory.py
@@ -6,3 +6,5 @@ def open_pdf(cursor, current_user_node):
     node = cursor.fetchall()
     subprocess.run(['start', '', node[0][2]], shell=True)
 
+
+
